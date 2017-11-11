2c965ca8908f78e7867d8af2f7ac07cfc6dedd62
1420671 <1420671@utp.edu.pe>
2017-11-11 13:12:17 -0500

    creando CreatePoll

---
+++ app/src/main/java/pe/edu/utp/knowledgemanagement/CreatePoll.java
@@ -0,0 +1,17 @@
+package pe.edu.utp.knowledgemanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hypnos on 11/11/2017.
+ */
+
+public class CreatePoll extends AppCompatActivity {
+    protected void onCreate (Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_poll);
+
+    }
+}
