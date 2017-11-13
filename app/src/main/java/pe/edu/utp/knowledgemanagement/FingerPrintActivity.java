bcb5fc6698267a55eec0408895f0d17992e5ec0e
Chrizthian <1521509@utp.edu.pe>
2017-11-12 21:19:22 -0500

    Adicion
    Layout y clase

---
+++ app/src/main/java/pe/edu/utp/knowledgemanagement/FingerPrintActivity.java
@@ -0,0 +1,16 @@
+package pe.edu.utp.knowledgemanagement;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FingerPrintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finger_print);
+
+    }
+
+}
