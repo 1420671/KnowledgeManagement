1a58e8858aa3c534dd9031d5791c546c308d458e
Chrizthian <1521509@utp.edu.pe>
2017-11-16 11:10:34 -0500

    Add
    Layout, color, strings and dimens; delet loguin

--- app/src/main/java/pe/edu/utp/knowledgemanagement/FingerPrintActivity.java
+++ app/src/main/java/pe/edu/utp/knowledgemanagement/FingerPrintActivity.java
@@ -3,13 +3,22 @@ package pe.edu.utp.knowledgemanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.security.KeyStore;
+
+import javax.crypto.Cipher;
 
 public class FingerPrintActivity extends AppCompatActivity {
+    private KeyStore keyStore;
+    private static final String KEY_NAME = "androidHive";
+    private Cipher cipher;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_finger_print);
+        setContentView(R.layout.activity_fingerprint);
 
     }
 
