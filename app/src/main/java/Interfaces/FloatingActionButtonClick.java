e4e53882eab46ec2700e08ec85e8b2f6c3f370f7
1420671 <1420671@utp.edu.pe>
2017-11-25 10:37:56 -0500

    para visualizar los cuestionarios

---
+++ app/src/main/java/Interfaces/FloatingActionButtonClick.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import android.view.View;
+
+/**
+ * Created by Hypnos on 25/11/2017.
+ */
+
+public interface FloatingActionButtonClick {
+    void onClick(View v, int position);
+}
